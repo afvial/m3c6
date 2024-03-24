d52e35365069c7685dd2d5c7f905e00489dca428
afvial <afvial@gmail.com>
2024-03-24 09:32:10 +0100

    Initial Commit

---
+++ guides/01_guide_pipenv.py
@@ -0,0 +1,3 @@
+# Guide to Pipenv for Managing a Python Project's Packages and Environment
+
+# No code
---
+++ guides/02_intro_classes_python.py
@@ -0,0 +1,10 @@
+# Introduction to Classes in Python
+
+class Invoice:
+
+  def greeting(self):
+    return 'Hi there'
+
+
+inv_one = Invoice()
+print(inv_one.greeting())
